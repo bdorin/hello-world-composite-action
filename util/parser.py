9080b10a08256526ffa6859bd9314445fe9570ae
bdorin <dorinioan.bob@hotmail.com>
2024-12-15 22:08:11 +0200

    tutorial

--- util/parser.py
+++ util/parser.py
@@ -4,6 +4,7 @@ import os
 import re
 
 log_file_path = os.getenv("LOG_PATH")
+eliminate_duplicates = os.getenv("ELIMINATE_DUPLICATES", "false").lower() == "true"
 
 # Error and warning patterns
 error_patterns = [
